40adf5e6191c7edbfc7e9b42e1eeed34fc149026
KarunyaR-git <karunya.offl98@gmail.com>
2026-06-04 09:55:55 +0530

    Error Middleware Refinement

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -10,7 +10,7 @@ async function getUsers(req, res) {
 
 }
 
-async function getUserById(req, res) {
+async function getUserById(req, res, next) {
     const id = req.params.id;
     if(mongoose.Types.ObjectId.isValid(id)) {
         //const user = users.find((user)=> user.id === id);
@@ -18,10 +18,12 @@ async function getUserById(req, res) {
         if(user) {
             res.status(200).json(user);
         } else {
-            res.status(404).send('User not found');
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            return next(error);
         }
     } else {
-        res.status(400).send('Invalid id format');
+        return res.status(400).send('Invalid id format');
     }    
 }
 
@@ -32,7 +34,7 @@ async function createUser(req, res, next) {
 
             res.status(201).json(user);
         } catch (error) {
-            next(error);
+            return next(error);
         }
 }
 
@@ -49,7 +51,9 @@ async function updateUser(req, res, next) {
                 runValidators: true
                 });
                 if (!updatedUser) {
-                    return res.status(404).send('User not found');
+                    const error = new Error('User not found');
+                    error.statusCode = 404;
+                    return next(error);                    
                 }
                 return res.status(200).send('Success');
             } catch(error) {
@@ -60,19 +64,21 @@ async function updateUser(req, res, next) {
     }
 }
 
-async function deleteUser(req, res) {
+async function deleteUser(req, res, next) {
     const id = req.params.id;
     if(mongoose.Types.ObjectId.isValid(id)) {
         const deleteUser = await User.findByIdAndDelete(id);
         console.log(deleteUser);
         if(!deleteUser) {            
-            return res.status(404).send('User not found');
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            return next(error);
         } 
         return res.status(204).end();
         //res.sendStatus(204); // Both will work just for reference adding here.
            
     } else {
-        res.status(400).send('Invalid id format');
+        return res.status(400).send('Invalid id format');
     }
 }
 
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,10 +1,11 @@
 function errorHandler (error, req, res, next) {
+    const statusCode = error.statusCode || 500;
     console.log(error)
     if (error.name === 'ValidationError' || error.name === 'CastError') {
         return res.status(400).send('Invalid user data');
     }
 
-    return res.status(500).send('Something went wrong');
+    return res.status(statusCode).send(error.message || "Something went wrong");
 }
 
 module.exports = errorHandler;
