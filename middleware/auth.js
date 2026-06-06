34c5d011e8778b49448ef42b3b1243fb22529a07
KarunyaR-git <karunya.offl98@gmail.com>
2026-06-06 11:08:38 +0530

    JWT verify authentication flow - Intro

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,27 @@
+const jwt = require('jsonwebtoken')
 function auth(req, res, next) {
-    const token = req.headers.token;
-    if(token === "123") {
-        next();
+    const secretKey = "familycare_secret";
+    const authHeader = req.headers.authorization;
+    const error = new Error('Unauthorized');
+    error.statusCode = 401;
+    if(authHeader){
+        if(authHeader.startsWith('Bearer')){
+            const token = authHeader.split(' ')[1];
+            try {
+                const verifiedUser = jwt.verify(token, secretKey);
+                req.user = verifiedUser;
+                next();
+            } catch(err) {
+                next(error);
+            }
+        } else {
+            next(error);
+        }
+        
     } else {
-        res.status(401).send("Unauthorized");
+        next(error);
     }
+    
 }
 
 module.exports = auth;
